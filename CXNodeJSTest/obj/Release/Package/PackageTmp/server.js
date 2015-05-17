d79162c2706e8d86af5106790e30e8d5901a9be7
gohawks3 <chuxu+github@hotmail.com>
2015-05-16 18:18:28 -0700

    play with node.js on vs2013 and publishing to azure

---
+++ CXNodeJSTest/obj/Release/Package/PackageTmp/server.js
@@ -0,0 +1,7 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World on the web from Chu! \n')
+    res.end('this is a test from node.js using VS2013');
+}).listen(port);
